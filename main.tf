596bf9b4739ba3b03dbc17406d5c8953a3205e52
Tatiana Espinola <espinola.data@gmail.com>
2026-01-07 21:17:41 -0300

    mudança de variavel de role e policy no attachment em main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_iam_policy" "iam_policy" {
 
 
 resource "aws_iam_role_policy_attachment" "example_attach" {
-  role       = aws_iam_role.example_role.name
-  policy_arn = aws_iam_policy.example_policy.arn
+  role       = aws_iam_role.iam_role.name
+  policy_arn = aws_iam_policy.iam_policy.arn
 }
 
